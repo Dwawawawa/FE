8f4822f1c55a5c5aed192038936186fda4b45dd9
Dwawawawa <gywpjang@naver.com>
2024-11-12 19:24:30 +0900

    Feat: ProtectorInfo 페이지 구현

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -3,6 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import BottomTabNavigator from '../components/common/BottomTabNavigator';
 import Landing from '../pages/Landing';
 import UserCategory from '../pages/UserCategory';
+import ProtectorInfo from '../pages/ProtectorInfo';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ const MainNavigator = () => {
     >
       <Stack.Screen name="Landing" component={Landing} />
       <Stack.Screen name="UserCategory" component={UserCategory} />
+      <Stack.Screen name="ProtectorInfo" component={ProtectorInfo} />
       <Stack.Screen name="Main" component={BottomTabNavigator} />
     </Stack.Navigator>
   );
